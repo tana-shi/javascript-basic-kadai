77ea413aa0977f24b5562ed5f48aefe71239242f
tana-shi <shingo_tana626@icloud.com>
2025-02-12 19:56:07 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,13 @@
+let bodyWeight;
+
+bodyWeight = "体重";
+
+let  bodyNumber=  68;
+
+let userHeight;
+
+userHeight = "身長";
+
+let heightNumber =  1.7;
+
+console.log(bodyNumber / heightNumber * heightNumber);
